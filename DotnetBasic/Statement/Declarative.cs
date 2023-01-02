9bac0d147304c36995de2ffae3f19c749ab92e55
irfan.manullang <ifmanullang@gmail.com>
2023-01-02 11:31:03 +0700

    belajar dotnet

---
+++ DotnetBasic/Statement/Declarative.cs
@@ -0,0 +1,15 @@
+﻿public class Declarative
+{
+    public static void Main()
+    {
+        Console.WriteLine("Declarative test");
+        //variable
+        double area = 0.0;
+        Console.WriteLine("Area value :" + area);
+        double radius = 2;
+        Console.WriteLine("Radius value: " + radius);
+        const double pi = 3.14;
+        Console.WriteLine("Pi value :" + pi);
+
+    }
+}
