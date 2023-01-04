8e630f314cd62a33fc5dba7596727037ade60ea8
irfan.manullang <ifmanullang@gmail.com>
2023-01-04 12:01:04 +0700

    polymhorphism

---
+++ DotnetBasic/Polymhorphism/MainPolymhorphism.cs
@@ -0,0 +1,23 @@
+﻿using Polymhorpism;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymhorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lingkaran LuasLingkaran = new Lingkaran(7);
+            Console.WriteLine("Luas Lingkaran = " + LuasLingkaran.HitungLuas());
+
+            Segitiga LuasSegitiga = new Segitiga(5, 9);
+            Console.WriteLine("Luas Segitiga = " + LuasSegitiga.HitungLuas());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DotnetBasic/Polymhorphism/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Polymhorpism
+{
+    
+        public abstract class HitungBagungDatar
+    {
+        public double luas;
+
+        public abstract double HitungLuas();
+    }
+     
+    public class Lingkaran : HitungBagungDatar
+    {
+        double JariJari;
+        public Lingkaran(double r)
+        {
+            JariJari = r;
+        }
+        public override double HitungLuas()
+        {
+            luas =  3.14 * JariJari * JariJari;
+            return luas;
+        }
+
+    }
+     
+    public class Segitiga : HitungBagungDatar
+    {
+        double alas, tinggi;
+        
+        public Segitiga(double a, double t)
+        {
+            alas = a;
+            tinggi = t;
+        }
+        public override double HitungLuas()
+        {
+            luas = 0.5 * alas * tinggi;
+            return luas;
+        }
+    }
+
+}
